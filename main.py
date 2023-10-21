90876bbac521ab7b16bbe17d42122fd1e52835dd
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-21 18:52:12 +0700

    Introduce cleanup of QRcode file after upload
    The changes include the import of Python's built-in 'os' module and the introduction of a command to remove the generated QRCode file immediately after it is uploaded to ImgBB. This is necessary to prevent accumulation of QRCode files in the working directory, ensuring efficient use of storage space. The unnecessary comment about the upload step was also removed.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, validator
 import pyqrcode
-from io import BytesIO
 import aiohttp
 import aiofiles
 
 app = FastAPI()
+
 
 class Item(BaseModel):
     url: str
@@ -16,6 +17,7 @@ class Item(BaseModel):
             raise ValueError("Invalid URL provided")
         return v
 
+
 @app.post("/generate_qrcode/")
 async def generate_qrcode(item: Item):
     try:
@@ -23,7 +25,6 @@ async def generate_qrcode(item: Item):
         filename = "qrcode.png"
         url.png(filename, scale=8)
 
-        #Step where we upload the QR code to imgBB
         async with aiohttp.ClientSession() as session:
             async with aiofiles.open(filename, mode='rb') as f:
                 binary_data = await f.read()
@@ -32,9 +33,11 @@ async def generate_qrcode(item: Item):
                     'name': 'qrcode',
                     'image': binary_data
                 }
-                response = await session.post('https://api.imgbb.com/1/upload?key=c793634661b542c39568732fe21627ff', data=data)
+                response = await session.post('https://api.imgbb.com/1/upload?key=c793634661b542c39568732fe21627ff',
+                                              data=data)
                 json_result = await response.json()
 
+        os.remove(filename)
         return {"qrcode": json_result}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
